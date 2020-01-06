37addc887b68d8bafe7ca12d454a289e66821c2d
Igor Tokarev <TigorC@gmail.com>
2020-01-06 14:46:47 +0500

    Update crel

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,20 @@
 module.exports = {
   input: './src/index.js',
   output: [{
-    file: 'dist/index.js',
+    file: 'dist/index.cjs.js',
     format: 'cjs',
     sourcemap: true
   }, {
     file: 'dist/index.es.js',
     format: 'es',
     sourcemap: true
+  }, {
+    file: 'dist/index.js',
+    name: 'prosemirror.menu',
+    format: 'umd',
+    sourcemap: true,
+    exports: 'named',
+    amd: { id: 'prosemirror-menu' }
   }],
   plugins: [require('@rollup/plugin-buble')()],
   external(id) { return !/^[\.\/]/.test(id) }
